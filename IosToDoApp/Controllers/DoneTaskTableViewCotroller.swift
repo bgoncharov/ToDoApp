77f434b0eb2d10bed508cd02d776ef5b6e16f387
Boris Goncharov <bora4bora@gmail.com>
2020-11-08 20:56:02 -0800

    View controllers

---
+++ IosToDoApp/Controllers/DoneTaskTableViewCotroller.swift
@@ -0,0 +1,16 @@
+//
+//  DoneTaskTableViewCotroller.swift
+//  IosToDoApp
+//
+//  Created by Boris Goncharov on 11/8/20.
+//
+
+import UIKit
+
+class DoneTaskTableViewCotroller: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+}
--- IosToDoApp/Controllers/TasksViewController.swift
+++ IosToDoApp/Controllers/TasksViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class TasksViewController: UIViewController {
 
     @IBOutlet weak var menuSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var doneTasksContainerView: UIView!
+    @IBOutlet weak var outgoingTasksContainerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,32 @@ class TasksViewController: UIViewController {
             menuSegmentedControl.insertSegment(withTitle: section.rawValue, at: index, animated: false)
         }
         menuSegmentedControl.selectedSegmentIndex = 0
+    }
+    
+    @IBAction func segmentedControlSwitched(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            segmentsSwitcher(for: .ongoing)
+        case 1:
+            segmentsSwitcher(for: .done)
+        default: break
+        }
+    }
+    
+    private func segmentsSwitcher(for section: MenuSection) {
+        switch section {
+        
+        case .ongoing:
+            doneTasksContainerView.isHidden = true
+            outgoingTasksContainerView.isHidden = false
+        case .done:
+            doneTasksContainerView.isHidden = false
+            outgoingTasksContainerView.isHidden = true
+        }
+    }
+    
+    @IBAction func addNewTaskTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "showAddNewTask", sender: nil)
     }
 }
 
